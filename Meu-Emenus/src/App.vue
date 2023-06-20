d2af2c4568241d91197aec51fb46ffea1d474aec
Vitor dos Santos Gomes <54445515+santosgv@users.noreply.github.com>
2023-06-20 18:58:42 -0300

    primeiro commit

---
+++ Meu-Emenus/src/App.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <header>
+    <img alt="Vue logo" class="logo" src="@/assets/icon.png" width="125" height="125" />
+
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+        <RouterLink to="/contato">contato</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+  <CNavbar color-scheme="light" class="bg-light">
+  <CContainer fluid>
+    <CNavbarBrand href="#">
+      <img src="@/assets/icon.png" alt="" width="50" height="50"/>
+    </CNavbarBrand>
+  </CContainer>
+</CNavbar>
+</template>
+
+<style scoped>
+</style>
---
+++ Meu-Emenus/src/views/contatoView.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+<template>
+<h1> teste</h1>
+</template>
+
+<style scoped>
+
+</style>
